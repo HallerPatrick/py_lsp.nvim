5655a902eb1d1235b6f2b700cd91bad30a125ab9
Dom Miketa <dominik.miketa@gmail.com>
2021-09-10 20:28:14 +0100

    remove use of completion in on_attach

--- lua/py_lsp/options.lua
+++ lua/py_lsp/options.lua
@@ -1,21 +1,17 @@
 local defaults = {
-	auto_source = true,
-	language_server = "pyright",
-	on_attach = require("completion").on_attach,
-	source_strategies = { "default", "poetry", "system" },
-	host_python = nil, -- this python should include the virtualenv module
+    auto_source = true,
+    language_server = "pyright",
+    on_attach = nil,
+    source_strategies = {"default", "poetry", "system"},
+    host_python = nil, -- this python should include the virtualenv module
 }
 
 local options = vim.deepcopy(defaults)
 
 local M = {}
 
-function M.set(user_opts)
-	options = vim.tbl_deep_extend("force", options, user_opts)
-end
+function M.set(user_opts) options = vim.tbl_deep_extend("force", options, user_opts) end
 
-function M.get()
-	return options
-end
+function M.get() return options end
 
 return M
