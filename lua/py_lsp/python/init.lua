760fc1c5335a3c10553375a64fa6f18920d61b67
Patrick Haller <phmaker@Patricks-MacBook-Pro.local>
2024-11-17 20:28:23 -0500

    Update some logic of using already activate venv in shell

--- lua/py_lsp/python/init.lua
+++ lua/py_lsp/python/init.lua
@@ -1,4 +1,7 @@
+local path = require("lspconfig/util").path
+
 local search_strategies = require("py_lsp.python.strategies")
+local options = require("py_lsp.options")
 
 local M = {
 	strategies = vim.tbl_keys(search_strategies),
@@ -9,12 +12,47 @@ M.strategy_provider = function(strategy)
 	return search_strategies[strategy]
 end
 
+
+---
+--- Check first env vars if we already have a activate venv
+--- TODO: We check probably add a option to check this first if the user
+--- wants to 
+---@param strategy string
+---@return string
+M.check_activated_env = function (strategy)
+  if strategy == "default" then
+    local default_venv_path = os.getenv("VIRTUAL_ENV")
+    if default_venv_path ~= "" then
+      return default_venv_path
+    end
+  elseif strategy == "conda" then
+    local conda_env_path = os.getenv("CONDA_PREFIX")
+    if conda_env_path ~= "" then
+      return path.join(conda_env_path, "bin", "python")
+    end
+  end
+
+  return nil
+end
+
 M.find_first_python_path = function(workspace, strategies, venv_name)
 	for _, strategy in ipairs(strategies) do
+        local activate_venv_path = M.check_activated_env(strategy)
+        if activate_venv_path ~= nil and options.get().default_venv_name == nil then
+          -- Check if path exists and normalize it
+          local expanded_path = vim.fn.expand(activate_venv_path)
+          if vim.fn.isdirectory(activate_venv_path) == 1 then
+            local absolute_path = vim.fn.fnamemodify(expanded_path, ':p')
+            return path.join(path.dirname(absolute_path), "bin", "python")
+          end
+
+          return activate_venv_path
+        end
 		local strategy_fn = M.strategy_provider(strategy)
 
 		if vim.is_callable(strategy_fn) then
 			local python_path = strategy_fn(workspace, venv_name)
+
 			if python_path ~= nil then
 				if type(python_path) == "table" then
 					return python_path[1]
--- lua/py_lsp/python/strategies.lua
+++ lua/py_lsp/python/strategies.lua
@@ -1,5 +1,9 @@
 local path = require("lspconfig/util").path
 
+function get_last_dir(path)
+    return path:match("([^/]+)$")
+end
+
 local M = {}
 
 M.default = function(workspace, venv_name)
@@ -45,15 +49,19 @@ M.poetry = function(workspace, _)
 	return nil
 end
 
-M.conda = function(_, _)
+M.conda = function(_, venv_name)
 	-- If no standard venv found look for conda environments
 	local found_envs = {}
 	local json_env_list = vim.fn.systemlist("$CONDA_EXE env list --json")
 	table.unpack = table.unpack or unpack -- 5.1 compatibility
 	local raw_env_list = { table.unpack(json_env_list, 3, #json_env_list - 2) }
-
 	for _, raw_env in ipairs(raw_env_list) do
 		local env = string.match(raw_env, '[^%s"]+')
+
+        if venv_name == get_last_dir(env) then
+          return path.join(env, "bin", "python")
+        end
+
 		table.insert(found_envs, path.join(env, "bin", "python"))
 	end
 
